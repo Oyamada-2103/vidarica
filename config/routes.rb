3ab15041ced2dd6e49a3310d134e4a060d5acfe9
Oyamada-2103 <ta9.syukatu@gmail.com>
2021-05-13 06:51:31 +0000

    [add]admin,tag,genre

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,30 @@ Rails.application.routes.draw do
       get :followers, on: :member
   end
 
+
+
+  # 管理者用サイトのrouting
+  devise_scope :admins do
+    devise_for :admins, controllers: {
+        registrations: 'admins/registrations',
+        passwords: 'admins/passwords',
+        sessions: 'admins/sessions'
+    }
+  end
+
+  namespace :admin do
+    # get 'homes/top' => 'homes#top', as:'top'
+    # resources :customers, only: [:index, :edit, :show, :update]
+    # resources :products, only: [:index, :create, :new, :edit, :show, :update]
+    # resources :orders, only: [:index, :create, :show, :update]
+    # resources :order_items, only: [:index, :create, :show, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :tags, only: [:index, :create, :edit, :update]
+    get 'search' => 'searches#search', as: 'search'
+  end
+
+
+
   get 'static_pages/home'
   # test後で消す
 
